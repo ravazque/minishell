cea448112bb9464901fe5a09d509ac8535bdbba6
ravazque <rk.raul1306@gmail.com>
2025-10-03 00:33:32 +0200

    minishell

--- src/aux_minishell/prompt.c
+++ src/aux_minishell/prompt.c
@@ -6,123 +6,275 @@
 /*   By: ravazque <ravazque@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 19:20:00 by ravazque          #+#    #+#             */
-/*   Updated: 2025/09/30 17:14:14 by ravazque         ###   ########.fr       */
+/*   Updated: 2025/10/03 00:19:37 by ravazque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static char	*basename_dup(const char *path)
+size_t	ft_strcspn(const char *s, const char *reject)
 {
-	size_t	len;
 	size_t	i;
-	size_t	start;
+	size_t	j;
 
-	if (!path)
-		return (ft_strdup("?"));
-	len = 0;
-	while (path[len])
-		len++;
-	while (len > 1 && path[len - 1] == '/')
-		len--;
-	start = 0;
-	if (len > 0)
+	i = 0;
+	while (s[i])
 	{
-		i = len;
-		while (i > 0 && path[i - 1] != '/')
-			i--;
-		start = i;
+		j = 0;
+		while (reject[j])
+		{
+			if (s[i] == reject[j])
+				return (i);
+			j++;
+		}
+		i++;
 	}
-	if (len == 1 && path[0] == '/')
-		return (ft_strdup("/"));
-	return (ft_substr(path, start, len - start));
+	return (i);
 }
 
-static char	*add_git_indicator(char *base)
+static int	can_access_path(const char *path)
 {
-	char	*with_git;
-	char	*tmp;
-	char	*tmp2;
-
-	tmp = ft_strjoin(RL_YEL, " [git]");
-	if (!tmp)
-		return (base);
-	tmp2 = ft_strjoin(tmp, RL_RST);
-	free(tmp);
-	if (!tmp2)
-		return (base);
-	with_git = ft_strjoin(base, tmp2);
-	free(tmp2);
-	if (!with_git)
-		return (base);
-	free(base);
-	return (with_git);
+	if (access(path, F_OK) != 0)
+		return (0);
+	if (access(path, R_OK) != 0)
+		return (0);
+	return (1);
 }
 
-static char	*rl_color_wrap(const char *s, const char *color)
+static char	*get_git_branch(const char *repo_path)
+{
+	char	*git_head_path;
+	char	*branch;
+	char	line[256];
+	FILE	*f;
+	char	*last_slash;
+
+	git_head_path = ft_strjoin(repo_path, "/.git/HEAD");
+	if (!git_head_path)
+		return (NULL);
+	if (!can_access_path(git_head_path))
+		return (free(git_head_path), NULL);
+	f = fopen(git_head_path, "r");
+	free(git_head_path);
+	if (!f)
+		return (NULL);
+	if (!fgets(line, sizeof(line), f))
+		return (fclose(f), NULL);
+	fclose(f);
+	line[ft_strcspn(line, "\n")] = '\0';
+	if (ft_strncmp(line, "ref: ", 5) == 0)
+	{
+		last_slash = ft_strrchr(line + 5, '/');
+		if (last_slash)
+			branch = ft_strdup(last_slash + 1);
+		else
+			branch = ft_strdup(line + 5);
+	}
+	else
+	{
+		if (ft_strlen(line) > 7)
+			branch = ft_substr(line, 0, 7);
+		else
+			branch = ft_strdup(line);
+	}
+	return (branch);
+}
+
+static char	*get_username(void)
+{
+	char	*user;
+
+	user = getenv("USER");
+	if (!user)
+		user = getenv("LOGNAME");
+	if (!user)
+		return (ft_strdup("user"));
+	return (ft_strdup(user));
+}
+
+static char	*get_hostname(void)
+{
+	char	hostname[256];
+
+	if (gethostname(hostname, sizeof(hostname)) == 0)
+		return (ft_strdup(hostname));
+	return (ft_strdup("localhost"));
+}
+
+static char	*get_short_path(const char *full_path)
+{
+	char	*home;
+	char	*short_path;
+	size_t	home_len;
+
+	if (!full_path)
+		return (ft_strdup("~"));
+	home = getenv("HOME");
+	if (home)
+	{
+		home_len = ft_strlen(home);
+		if (ft_strncmp(full_path, home, home_len) == 0)
+		{
+			if (full_path[home_len] == '\0')
+				return (ft_strdup("~"));
+			if (full_path[home_len] == '/')
+			{
+				short_path = ft_strjoin("~", full_path + home_len);
+				return (short_path);
+			}
+		}
+	}
+	return (ft_strdup(full_path));
+}
+
+static char	*build_user_host(void)
+{
+	char	*user;
+	char	*host;
+	char	*user_host;
+	char	*tmp;
+	char	*colored;
+
+	user = get_username();
+	host = get_hostname();
+	if (!user || !host)
+	{
+		if (user)
+			free(user);
+		if (host)
+			free(host);
+		return (NULL);
+	}
+	tmp = ft_strjoin(user, "@");
+	free(user);
+	if (!tmp)
+		return (free(host), NULL);
+	user_host = ft_strjoin(tmp, host);
+	free(tmp);
+	free(host);
+	if (!user_host)
+		return (NULL);
+	tmp = ft_strjoin(RL_GRN, "[");
+	if (!tmp)
+		return (free(user_host), NULL);
+	colored = ft_strjoin(tmp, user_host);
+	free(tmp);
+	free(user_host);
+	if (!colored)
+		return (NULL);
+	tmp = ft_strjoin(colored, "]");
+	free(colored);
+	if (!tmp)
+		return (NULL);
+	colored = ft_strjoin(tmp, RL_RST);
+	free(tmp);
+	return (colored);
+}
+
+static char	*build_path_section(const char *pwd)
+{
+	char	*short_path;
+	char	*colored;
+	char	*tmp;
+
+	short_path = get_short_path(pwd);
+	if (!short_path)
+		return (NULL);
+	tmp = ft_strjoin(RL_BLU, short_path);
+	free(short_path);
+	if (!tmp)
+		return (NULL);
+	colored = ft_strjoin(tmp, RL_RST);
+	free(tmp);
+	return (colored);
+}
+
+static char	*build_git_section(const char *branch)
+{
+	char	*git_part;
+	char	*tmp;
+	char	*colored;
+
+	if (!branch)
+		return (ft_strdup(""));
+	tmp = ft_strjoin(" git:(", branch);
+	if (!tmp)
+		return (ft_strdup(""));
+	git_part = ft_strjoin(tmp, ")");
+	free(tmp);
+	if (!git_part)
+		return (ft_strdup(""));
+	tmp = ft_strjoin(RL_RED, git_part);
+	free(git_part);
+	if (!tmp)
+		return (ft_strdup(""));
+	colored = ft_strjoin(tmp, RL_RST);
+	free(tmp);
+	if (!colored)
+		return (ft_strdup(""));
+	return (colored);
+}
+
+static char	*join_all_parts(char *user_host, char *path, char *git)
 {
 	char	*tmp1;
 	char	*tmp2;
+	char	*result;
 
-	tmp1 = ft_strjoin(color, s);
+	tmp1 = ft_strjoin(user_host, " ");
 	if (!tmp1)
 		return (NULL);
-	tmp2 = ft_strjoin(tmp1, RL_RST);
+	tmp2 = ft_strjoin(tmp1, path);
 	free(tmp1);
-	return (tmp2);
-}
-
-static char	*build_prompt(const char *shown, int has_git)
-{
-	char	*colored;
-	char	*with_git;
-	char	*prompt;
-
-	colored = rl_color_wrap(shown, RL_CYN);
-	if (!colored)
+	if (!tmp2)
 		return (NULL);
-	if (has_git)
-	{
-		with_git = add_git_indicator(colored);
-		prompt = ft_strjoin(with_git, " $ ");
-		free(with_git);
-	}
-	else
-		prompt = ft_strjoin(colored, " $ ");
-	if (colored && !has_git)
-		free(colored);
-	return (prompt);
+	tmp1 = ft_strjoin(tmp2, git);
+	free(tmp2);
+	if (!tmp1)
+		return (NULL);
+	result = ft_strjoin(tmp1, " $ ");
+	free(tmp1);
+	return (result);
 }
 
 char	*prompt(t_mini *mini)
 {
-	char	*shown;
-	char	*prompt;
-	int		has_git;
+	char	*user_host;
+	char	*path_section;
+	char	*git_section;
+	char	*branch;
+	char	*final_prompt;
 
 	if (!mini)
 		return (NULL);
 	if (mini->pwd)
-	{
 		free(mini->pwd);
-		mini->pwd = NULL;
-	}
 	mini->pwd = getcwd_or_pwd();
 	if (!mini->pwd)
 		return (NULL);
-	shown = basename_dup(mini->pwd);
-	if (!shown)
-		return (NULL);
-	has_git = is_git_repo(mini->pwd);
-	prompt = build_prompt(shown, has_git);
-	free(shown);
-	return (prompt);
+	user_host = build_user_host();
+	path_section = build_path_section(mini->pwd);
+	branch = NULL;
+	if (is_git_repo(mini->pwd))
+		branch = get_git_branch(mini->pwd);
+	git_section = build_git_section(branch);
+	if (branch)
+		free(branch);
+	if (!user_host || !path_section || !git_section)
+	{
+		if (user_host)
+			free(user_host);
+		if (path_section)
+			free(path_section);
+		if (git_section)
+			free(git_section);
+		return (ft_strdup("$ "));
+	}
+	final_prompt = join_all_parts(user_host, path_section, git_section);
+	free(user_host);
+	free(path_section);
+	free(git_section);
+	if (!final_prompt)
+		return (ft_strdup("$ "));
+	return (final_prompt);
 }
-
-// static char	*basename_dup(const char *path)
-// {
-// 	if (!path)
-// 		return (ft_strdup("?"));
-// 	else
-// 		return (ft_strdup(path));
-// }
