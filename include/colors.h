2703185bc6774b3b33ee2b4cbaeda40341f83085
ravazque <rk.raul1306@gmail.com>
2025-10-13 18:36:49 +0200

    all

--- include/colors.h
+++ include/colors.h
@@ -6,27 +6,27 @@
 /*   By: ravazque <ravazque@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 01:35:17 by ravazque          #+#    #+#             */
-/*   Updated: 2025/10/03 02:04:43 by ravazque         ###   ########.fr       */
+/*   Updated: 2025/10/13 18:23:16 by ravazque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COLORS_H
 # define COLORS_H
 
-# define CLR_RESET "\033[0m"
-# define CLR_BOLD "\033[1m"
-# define CLR_RED "\033[31m"
-# define CLR_GRN "\033[32m"
-# define CLR_YEL "\033[33m"
-# define CLR_BLU "\033[34m"
-# define CLR_MAG "\033[35m"
-# define CLR_CYN "\033[36m"
-# define CLR_WHT "\033[37m"
+# define CLR_RESET "\001\033[0m\002"
+# define CLR_BOLD "\001\033[1m\002"
+# define CLR_RED "\001\033[31m\002"
+# define CLR_GRN "\001\033[32m\002"
+# define CLR_YEL "\001\033[33m\002"
+# define CLR_BLU "\001\033[34m\002"
+# define CLR_MAG "\001\033[35m\002"
+# define CLR_CYN "\001\033[36m\002"
+# define CLR_WHT "\001\033[37m\002"
 
 # define RL_S "\001"
 # define RL_E "\002"
 
-# define BOLD_GREEN "\033[1;32m"
+# define BOLD_GREEN "\001\033[1;32m\002"
 
 # define RL_RST "\001\033[0m\002"
 # define RL_BLD "\001\033[1m\002"
